939209ddcc91c0fa57511b44c17d662cd06b851c
Иван <ivankiseliov@rambler.ru>
2022-09-26 22:27:07 +0300

    дз 4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //массив 1
@@ -5,32 +7,15 @@ public class Main {
         massive1[0] = 1;
         massive1[1] = 2;
         massive1[2] = 3;
-        //массив 2
-        double[] massive2 = {1.57, 7.654, 9.986};
-        //массив 3
-        int[] massive3 = {5, 10, 15, 20};
-        //вывели первый
-        for (int i = massive1.length - 1; i >= 0; i--) {
+        for (int i = 0; i < massive1.length; i++) {
+            if (massive1[i] % 2 != 0) {
+                massive1[i] += 1;
+            }
             System.out.print(massive1[i]);
-            if (i != 0) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-        //вывели второй
-        for (double a = massive2.length - 1; a >= 0; a--) {
-            System.out.print(massive2[(int) a]);
-            if (a != 0) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-        //вывели третий
-        for (int b = massive3.length - 1; b >=0; b--) {
-            System.out.print(massive3[b]);
-            if (b != 0) {
+            if (i != massive1.length - 1) {
                 System.out.print(", ");
             }
         }
     }
 }
+
