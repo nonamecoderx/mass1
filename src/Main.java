2be4eaa0e91c4d86720a7020205d94127d4282ac
Иван <ivankiseliov@rambler.ru>
2022-09-27 07:05:20 +0300

    дз 1-2

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
         double[] massive2 = {1.57, 7.654, 9.986};
         //массив 3
         int[] massive3 = {5, 10, 15, 20};
-        //вывели первый
         for (int i = 0; i < massive1.length; i++) {
             System.out.print(massive1[i]);
             if (i != massive1.length - 1) {
@@ -17,7 +16,6 @@ public class Main {
             }
         }
         System.out.println();
-        //вывели второй
         for (double a = 0; a < massive2.length; a++) {
             System.out.print(massive2[(int) a]);
             if (a != massive1.length - 1) {
@@ -25,7 +23,6 @@ public class Main {
             }
         }
         System.out.println();
-        //вывели третий
         for (int b = 0; b < massive3.length; b++) {
             System.out.print(massive3[b]);
             if (b != massive3.length - 1) {
